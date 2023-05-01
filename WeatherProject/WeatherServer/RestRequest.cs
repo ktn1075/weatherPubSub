9cee870df5fd6e42868752d00c5185c93f275492
taenyen kim <ktn1075@a.ut.ac.kr>
2023-05-01 22:36:11 +0900

    Rest/api 수정

--- WeatherProject/WeatherServer/RestRequest.cs
+++ WeatherProject/WeatherServer/RestRequest.cs
@@ -1,9 +1,12 @@
 ﻿using Newtonsoft.Json;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.XPath;
 using WatsonWebserver;
 
 class RestRequest
@@ -11,6 +14,8 @@ class RestRequest
     private static Server _server = null;
     private static readonly int REST_PORT = 50000;
     private static readonly string REST_ADDRESS = "127.0.0.1";
+
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
     public static string Start()
     {
@@ -27,6 +32,7 @@ class RestRequest
         catch (Exception e)
         {
             result = e.Message;
+            _logger.Error(e);
         }
 
         return result;
@@ -40,12 +46,34 @@ class RestRequest
         {
             Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
 
-            var t = JsonConvert.DeserializeObject<Dictionary<string, string>>(ctx.Request.DataAsString);
+            bool result = true;
 
+            try
+            {
+                var responsedata = JsonConvert.DeserializeObject<Dictionary<string, string>>(ctx.Request.DataAsString);
+
+                if (!responsedata.ContainsKey("subArea"))
+                {
+                    throw new Exception("Parameter error");
+                }
+   
+            }
+            catch(Exception e) 
+            {
+                _logger.Error(e);
+                result = false;
+            }
+           
+            await ctx.Response.Send(JsonConvert.SerializeObject(new { success = result }));
+        });
+
+        // agent 삭제
+        _server.Routes.Static.Add(HttpMethod.POST, "/agent/delete", async (ctx) =>
+        {
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+
+            // 삭제 코드 등록
             
-
-
-            await ctx.Response.Send(JsonConvert.SerializeObject(keyValuePairs));
         });
     }
 }
